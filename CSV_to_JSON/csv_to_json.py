89c9145b4dcd55f6e122800dd1737de9d52f5f1b
TechBoyy6 <techboyy6@gmail.com>
2021-10-06 13:27:28 +0530

    Added CSV_to_JSON

---
+++ CSV_to_JSON/csv_to_json.py
@@ -0,0 +1,33 @@
+import csv
+import json
+
+file_name = input("Provide the CSV filename without extension>> ")
+
+
+
+with open(file_name+'.csv') as f:
+
+    reader = csv.reader(f, delimiter=',')
+
+    titles = []
+    temp_data = {}
+
+    for heading in reader:
+        print(heading)
+        titles = heading
+        break
+
+    i = 1
+    
+    for row in reader:
+        print(row)
+        current_row = "row{}".format(i)
+        temp_data['{}'.format(current_row)] = {}
+        for col in range(len(titles)):
+            temp_data[current_row][titles[col]] = row[col]
+        i+=1
+
+with open(file_name+'.json', 'w') as f_j:
+    json.dump(temp_data, f_j, indent=4)
+
+print("File converted successfully :)")
